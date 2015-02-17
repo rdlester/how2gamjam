a6cb777317aa24ed4243611aca213244b4475058
rdlester <sursiks.010@gmail.com>
2015-02-16 16:02:29 -0800

    this is actually starting to look good

--- tools/cleaner.py
+++ tools/cleaner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import nltk
 import os
 import re
 import sys
@@ -7,28 +8,39 @@ corpusFileName = 'data/corpus.txt'
 cleanedCorpusName = 'data/cleanCorpus.txt'
 
 # remove weirdly formatted manual RTs, note those kleene stars, no +
-rtRe = re.compile('^(RT\s*)?\w*:')
+rtRe = re.compile('^(RT\s*)?\w*:', re.I)
 
 # who is tweeting this junk
-dstRe = re.compile(r'(\#DaylightSavingsTime)|(\#DaylightSavingTime)')
+dstRe = re.compile(r'(\#DaylightSavingsTime)|(\#DaylightSavingTime)', re.I)
 
-# drop hashtags that occur at end of line
+# drop hashtags that occur at beginning and end of line
 # Q: drop these entirely? clear sign of obnoxious #relevant game tweets
 startHashtagRe = re.compile(r'^(\#\w+\s+){2,}')
 endHashtagRe = re.compile(r'(\s*[\-\–]\s*)?(\#\w+\s*)+$')
+
+# there is some funky via usage, hence all the \s
+viaRe = re.compile(r'(via|by)\s*\@\s*\w+\s*', re.I)
+hangingViaRe = re.compile(r'\s*(via|by)\s*$', re.I)
+hangingAtRe = re.compile(r'\s*\@\s*\w+\s*$')
 
 def cleanCorpus():
   with open(corpusFileName, mode='r') as fraw:
     with open(cleanedCorpusName, mode='w') as fclean:
       for line in fraw:
         line = cleanLine(line)
-        if line is None or line is '':
+        if line is None or line == '':
           continue
         fclean.write(line)
         fclean.write('\n')  # does line already contain \n?
 
 # returns cleaned line or None if line should be excluded.
+pastLines = set()
 def cleanLine(line):
+  # prevent repeated tweets
+  if line in pastLines:
+    return None
+  pastLines.add(line)
+
   # exclude offenders
   if reCollide(rtRe, line) or reCollide(dstRe, line):
     return None
@@ -36,11 +48,18 @@ def cleanLine(line):
   #remove junk
   line = startHashtagRe.sub('', line)
   line = endHashtagRe.sub('', line)
+  line = viaRe.sub('', line)
+  line = hangingViaRe.sub('', line)
+  # line = hangingAtRe.sub('', line)
 
   # fix formatting
   line = line.replace('\n', '')
   line = line.replace ('...', '')
   line = line.strip()
+
+  # drop tweets with only a single word left.
+  if len(nltk.word_tokenize(line)) == 1:
+    return None
 
   return line
 
--- tools/runMarkov.py
+++ tools/runMarkov.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
 from ebook.markov import MarkovGenerator, twitter_tokenize
+import random
+import re
+
+# get rid of annoying trailing ands.
+andRe = re.compile(r'\s(and|the).$', re.I)
 
 if __name__ == "__main__":
   with open('data/cleanCorpus.txt', mode='r') as fin:
     with open('data/markov-gamedev.txt', mode='a') as fout:
       tweetCorpus = fin.read()
       mc = MarkovGenerator(tweetCorpus, 90, tokenize_fun=twitter_tokenize)
+      lengths = []
       for x in range(50):
-        fout.write(mc.generate_words())
+        mc.length = min(25 + round(abs(random.gauss(0, 75))), 130)
+        print(mc.length)
+        lengths.append(mc.length)
+        tweet = mc.generate_words()
+
+        # exclude @ replies!
+        if tweet.find('@') != -1:
+          continue
+
+        tweet = andRe.sub('.', tweet)
+
+        fout.write(tweet)
         fout.write('\n')
+
+      print('Average')
+      print(sum(lengths) / len(lengths))
