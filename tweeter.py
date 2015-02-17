d30604a090b691bd26bac435d31da1dcd2c9e0d1
rdlester <sursiks.010@gmail.com>
2015-02-16 17:36:31 -0800

    i spoke too soon

--- tweeter.py
+++ tweeter.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from ebook.markov import MarkovGenerator, twitter_tokenize
+from math import log
 import os
-from random import random
+from random import gauss, random
 import re
+import sys
 import time
 from twython import Twython, TwythonError
 from wordfilter.lib.wordfilter import Wordfilter
@@ -24,14 +26,15 @@ min_interval = 300
 average_interval = 7200
 
 def loadModel():
+  global generator
   with open('data/cleanCorpus.txt') as f:
     tweetCorpus = f.read()
     generator = MarkovGenerator(tweetCorpus, 0, tokenize_fun=twitter_tokenize)
-  return mc
+    print("model loaded")
 
-def publishTweet():
+def generateTweet():
   # TODO: this works but i should use a real distribution (?)
-  generator.length = min(25 + round(abs(random.gauss(0, 75))), 130)
+  generator.length = min(25 + round(abs(gauss(0, 75))), 130)
   tweet = generator.generate_words()
 
   # exclude undesired tweets
@@ -43,24 +46,22 @@ def publishTweet():
   # clean up random markov junk
   tweet = andRe.sub('.', tweet)
 
-  #tweet and log
-  twitter.update_status(tweet)
-
   # return tweet
   return tweet
 
 if __name__ == '__main__':
   loadModel()
   if generator is None:
-    return
+    sys.exit("failure loading corpus")
 
   while True:
-    tweet = publishTweet()
+    tweet = generateTweet()
 
     # immediately generate a new tweet if we failed
     if tweet is None:
       continue
 
-    # log and delay
+    # tweet, log and delay
+    twitter.update_status(status=tweet)
     print(time.strftime('[%y-%m-%dT%H:%M:%S] {}').format(tweet))
     time.sleep(min_interval - log(random())) * (average_interval - min_interval)
